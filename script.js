52f23cf785fdff7cb55ede7b509c034f6cfeb2cc
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-05-23 19:38:03 +0530

    done till 12.50

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage= new Image();
 playerImage.src='shadow_dog.png';
+const spriteWidth=575;
+const spriteHeight=523;
 
+// 5230 - is full sheet height, 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.fillRect(50,50,100,100);
-    ctx.drawImage(playerImage,0,0);
+    // ctx.fillRect(50,50,100,100);
+    // ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw,dh)
+    // sx - source image x , sy - source y, sw - source width, sh - source height, dx - destination x, dy - destination y, dw - destination width, dh - destination height
+    
+    ctx.drawImage(playerImage,0,0,spriteWidth,spriteHeight,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     requestAnimationFrame(animate);
 }
 

8ef924a077002ded7aa039e85c48f939a42c9116
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-05-23 20:37:06 +0530

    29.05

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ playerImage.src='shadow_dog.png';
 const spriteWidth=575;
 const spriteHeight=523;
 let frameX=0;
-let frameY=0;
+let frameY=4;
 let gameFrame=0;
 const staggerFrames=5;
 // higher stagger frames, slower the animation will be
@@ -20,11 +20,11 @@ const staggerFrames=5;
 // 5230 - is full sheet height, 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let position=Math.floor(gameFrame/staggerFrames)%6;
+    let position=Math.floor(gameFrame/staggerFrames)%11;
 
     frameX = spriteWidth * position;
 
-    ctx.drawImage(playerImage,frameX,frameY,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+    ctx.drawImage(playerImage,frameX,frameY*spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
    
     gameFrame++;
     requestAnimationFrame(animate);
